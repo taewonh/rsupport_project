6804a7a3ea0980c331cd03c98c39060e5a101539
taewon.hong <40269387+taewonh@users.noreply.github.com>
2025-06-28 14:46:36 +0900

    test: add test case for save announcement

---
+++ src/test/java/com/taewon/project/rsupport_test/announcement/AnnouncementRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.taewon.project.rsupport_test.announcement;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@DataJpaTest
+public class AnnouncementRepositoryTest {
+
+    @Autowired
+    private AnnouncementRepository repository;
+
+    @Test
+    public void 공지사항_저장() {
+
+        Announcement announcement = Announcement.insertBuilder()
+                .title("테스트 공지사항 제목")
+                .description("테스트 공지사항 내용")
+                .start_expose_time(LocalDateTime.now())
+                .end_expose_time(LocalDateTime.now())
+                .build();
+        Assertions.assertNotNull(announcement);
+        Announcement registered = repository.save(announcement);
+
+        Long id = registered.getId();
+        Assertions.assertEquals(1L, id);
+
+        List<Announcement> announcements = repository.findAll();
+        Assertions.assertEquals(1, announcements.size());
+    }
+}
