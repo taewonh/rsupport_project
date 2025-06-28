790943abf2752b9eb936aafe3c49a4bbb2380c54
taewon.hong <40269387+taewonh@users.noreply.github.com>
2025-06-28 14:44:27 +0900

    refactor(Announcement): entity 객체 필드 추가 및 보완
    - 기본키 생성 전략 적용
    - 신규 필드 (deleted, created_at, update_at, deleted_at) 추가
    - 기존 필드 명칭 변경 (start_expose_time, end_expose_time)

--- src/main/java/com/taewon/project/rsupport_test/announcement/Announcement.java
+++ src/main/java/com/taewon/project/rsupport_test/announcement/Announcement.java
@@ -1,21 +1,72 @@
 package com.taewon.project.rsupport_test.announcement;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
-@Builder
+@Entity(name = "announcement")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
 public class Announcement {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String title;
 
     private String description;
 
-    private Timestamp start_at;
+    private Boolean deleted;
 
-    private Timestamp end_at;
+    @Column(name = "start_expose_time")
+    private LocalDateTime start_expose_time;
+
+    @Column(name = "end_expose_time")
+    private LocalDateTime end_expose_time;
+
+    @Column(name = "created_at")
+    private Timestamp createdAt;
+
+    @Column(name = "updated_at")
+    private Timestamp updatedAt;
+
+    @Column(name = "deleted_at")
+    private Timestamp deletedAt;
 
     // TODO 첨부파일 목록은 별도 리스트로 관리 필요
+
+    @Builder(builderClassName = "InsertBuilder", builderMethodName = "insertBuilder")
+    public Announcement(String title,
+                        String description,
+                        LocalDateTime start_expose_time,
+                        LocalDateTime end_expose_time) {
+        this.title = title;
+        this.description = description;
+        this.start_expose_time = start_expose_time;
+        this.end_expose_time = end_expose_time;
+        this.deleted = false;
+    }
+
+    public void delete() {
+        this.deleted = true;
+        this.deletedAt = new Timestamp(System.currentTimeMillis());
+    }
+
+    @PrePersist
+    void createdAt() {
+        this.createdAt = new Timestamp(System.currentTimeMillis());
+        this.updatedAt = this.createdAt;
+    }
+
+    @PreUpdate
+    void updatedAt() {
+        this.updatedAt = new Timestamp(System.currentTimeMillis());
+    }
 }
